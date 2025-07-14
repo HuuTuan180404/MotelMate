450dc53021ce62b52265a5cfdb4c13af916650aa
anhhoang69 <anh.hoangngoc200404@gmail.com>
2025-07-14 11:38:07 +0700

    Add routes

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Listinvoice } from './pages/listinvoice/listinvoice';
+import { RoomManagement } from './pages/roommanagement/roommanagement';
+import { Layout } from './layout/layout';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: 'invoices', component: Listinvoice },
+      { path: 'rooms', component: RoomManagement },
+    ]
+  }
+];
--- FRONTEND/src/app/layout/layout.ts
+++ FRONTEND/src/app/layout/layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Sidebar } from './sidebar/sidebar';
 import { Header } from './header/header';
 import { RoomManagement } from '../pages/roommanagement/roommanagement';
 
 @Component({
   selector: 'app-layout',
-  imports: [Sidebar, Header, RoomManagement],
+  imports: [Sidebar, Header, RouterModule],
   templateUrl: './layout.html',
   styleUrls: ['./layout.css'],
 })
