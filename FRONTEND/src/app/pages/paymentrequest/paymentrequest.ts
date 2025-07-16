10bde2069866030bc4301ccabd2026feefda686a
Nguyễn Hữu Tuấn <nguyenhuutuan1704@gmail.com>
2025-07-16 16:42:18 +0700

    Squashed commit of the following:
    commit d8cdd8cce59b24572f045d9d6617b496f64f2adf
    Author: anhhoang69 <anh.hoangngoc200404@gmail.com>
    Date:   Wed Jul 16 16:36:17 2025 +0700
    
        update invoice
    
    commit df340f40756278be517e5f8f6fdb0187a9f1fbb1
    Merge: 0e78ed6 0d162e9
    Author: anhhoang69 <anh.hoangngoc200404@gmail.com>
    Date:   Wed Jul 16 14:19:46 2025 +0700
    
        Resolve merge conflict between anh and origin/tri
    
    commit 0e78ed6c9204f8d398c6ac138544772d72dacd61
    Author: anhhoang69 <anh.hoangngoc200404@gmail.com>
    Date:   Wed Jul 16 14:08:03 2025 +0700
    
        add invoice detail

---
+++ FRONTEND/src/app/pages/paymentrequest/paymentrequest.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentrequest',
+  imports: [],
+  templateUrl: './paymentrequest.html',
+  styleUrl: './paymentrequest.css'
+})
+export class Paymentrequest {
+
+}
