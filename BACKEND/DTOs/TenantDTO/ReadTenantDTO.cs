81f2445f6f0dbfcdf18b2c883e6c02e3c3843f1b
Nguyễn Hữu Tuấn <nguyenhuutuan1704@gmail.com>
2025-07-24 06:37:58 +0700

    refactor(db): change DateTime to DateOnly for dates

--- BACKEND/DTOs/TenantDTO/ReadTenantDTO.cs
+++ BACKEND/DTOs/TenantDTO/ReadTenantDTO.cs
@@ -7,7 +7,7 @@ namespace BACKEND.DTOs.RoomDTO
         public int TenantID { get; set; }
         public string FullName { get; set; }
         public required string CCCD { get; set; }
-        public DateTime Bdate { get; set; }
+        public DateOnly Bdate { get; set; }
         public string PhoneNumber { get; set; }
         public string Status { get; set; }
         public string? URLAvatar { get; set; }
