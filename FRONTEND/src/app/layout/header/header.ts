9080423ba1526b91d77f4a3a678f8712d4f725c3
viettri2004 <tonviettri2004@gmail.com>
2025-08-04 21:56:03 +0700

    feat(header): integrate dynamic profile data with ProfileService

--- FRONTEND/src/app/layout/header/header.ts
+++ FRONTEND/src/app/layout/header/header.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ProfileService } from './../../services/profileservice';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { SendNotification } from '../../pages/sendrequest/sendnotification';
 import { Observable } from 'rxjs';
@@ -11,24 +12,32 @@ import { Profile } from '../../pages/profile/profile';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit {
   lastNotification: any = null;
-
+  profileImage = '../../assets/images/avatar_error.png';
+  profileName = '';
   constructor(
     private dialogSendRequest: MatDialog,
-    private dialogProfile: MatDialog
+    private dialogProfile: MatDialog,
+    private profileService: ProfileService
   ) {}
-
+  ngOnInit(): void {
+    this.refreshProfile();
+  }
   sendMessage() {
     this.openNotificationPopup();
   }
 
   openProfileDialog(): void {
-    this.dialogProfile.open(Profile, {
+    const dialogRef = this.dialogProfile.open(Profile, {
       width: '60%',
       maxWidth: '60rem',
       maxHeight: '90vh',
       panelClass: 'custom-dialog',
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.refreshProfile();
     });
   }
 
@@ -47,7 +56,13 @@ export class Header {
       }
     });
   }
-
+  refreshProfile() {
+    this.profileService.getProfile().subscribe((data) => {
+      this.profileImage =
+        data.urlAvatar || '../../assets/images/avatar_error.png';
+      this.profileName = data.fullName;
+    });
+  }
   showSuccessMessage(): void {
     alert('Gửi thông báo thành công!');
   }
