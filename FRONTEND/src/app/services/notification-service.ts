dc17ad99b76a40988aa6d1c3961d0948eeeb52e4
Nguyễn Hữu Tuấn <nguyenhuutuan1704@gmail.com>
2025-08-04 10:57:47 +0700

    feat(notification): implement send notification functionality

---
+++ FRONTEND/src/app/services/notification-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  private api = `${environment.apiUrl}/api`;
+  constructor(private http: HttpClient) {}
+
+  sendNotification(data: any): Observable<any> {
+    return this.http.post(`${this.api}/Notification/send-notification`, data);
+  }
+}
